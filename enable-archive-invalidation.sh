20d2106c2393bd5f06426f28d1d6fce49f569596
katarafett <arpperry19@gmail.com>
2024-09-01 16:51:23 -0400

    enable-archive-... working, but is super bad. pending rewrite

--- enable-archive-invalidation.sh
+++ enable-archive-invalidation.sh
@@ -1,33 +1,50 @@
-#! /bin/zsh
+#! /bin/bash
 
-starfieldcustom="StarfieldCustom.ini"
+starfield_custom="StarfieldCustom.ini"
 docs_folder=$1
 
-echo $docs_folder
-cd "$docs_folder"
+echo "$docs_folder"
+cd "$docs_folder" || exit
 
-if ! [ -e $starfieldcustom ]; then
-    echo "Creating $starfieldcustom"
-    touch $starfieldcustom
+if ! [ -e $starfield_custom ]; then
+    echo "Creating $starfield_custom"
+    touch $starfield_custom
 fi
 
-if [ "$(grep -i "\[Archive\]" $starfieldcustom -c)" = "0" ]; then
+if [ "$(grep -i "\[Archive\]" $starfield_custom -c)" = "0" ]; then
     echo "File contains no [Archive] section; generating"
-    printf "[Archive]\nbInvalidateOlderFiles=1\nsResourceDataDirsFinal=\n" > $starfieldcustom
-    return 0
+    printf "[Archive]\nbInvalidateOlderFiles=1\nsResourceDataDirsFinal=\n" >> $starfield_custom
+    exit
 fi
 
+binv_set=false
+sres_set=false
 current_section=""
 line_number=1
 while read -r line; do
-    if ! [ "$(grep -i "\[[a-zA-Z0-9]*]" <<< $line -c)" = "0" ]; then
-        current_section=$(grep -i "\[[a-zA-Z0-9]*]" <<< $line -o)
-    elif [ "${current_section:l}" = "[archive]" ]; then
-        if ! [ "$(grep -i "bInvalidateOlderFiles=.*" <<< $line -c)" = "0" ]; then
-            sed -i "${line_number}s/.*/bInvalidateOlderFiles=1/" $starfieldcustom
-        elif ! [ "$(grep -i "sResourceDataDirsFinal=.*" <<< $line -c)" = "0" ]; then
-            sed -i "${line_number}s/.*/sResourceDataDirsFinal=/" $starfieldcustom
+    # Get the current section being read
+    if ! [ "$(grep -i "\[[a-zA-Z0-9]*]" <<< "$line" -c)" = "0" ]; then
+        current_section=$(grep -i "\[[a-zA-Z0-9]*]" <<< "$line" -o)
+    # Search archive invalidation lines and correct them if the exist
+    elif [ "${current_section,,}" = "[archive]" ]; then
+        if ! [ "$(grep -i "bInvalidateOlderFiles=.*" <<< "$line" -c)" = "0" ]; then
+            sed -i "${line_number}s/.*/bInvalidateOlderFiles=1/" $starfield_custom
+            binv_set=true
+        elif ! [ "$(grep -i "sResourceDataDirsFinal=.*" <<< "$line" -c)" = "0" ]; then
+            sed -i "${line_number}s/.*/sResourceDataDirsFinal=/" $starfield_custom
+            sres_set=true
         fi
     fi
     line_number=$((line_number+1))
-done < $starfieldcustom
+done < $starfield_custom
+
+if [ $binv_set = false ]; then
+    section_start=$(grep -n -i "\[archive\]" $starfield_custom | grep "^." -o)
+    sed -i "${section_start}s/.*/[Archive]\nbInvalidateOlderFiles=1/" $starfield_custom
+    binv_set=true
+fi
+if [ $sres_set = false ]; then
+    section_start=$(grep -n -i "\[archive\]" $starfield_custom | grep "^." -o)
+    sed -i "${section_start}s/.*/[Archive]\nsResourceDataDirsFinal=/" $starfield_custom
+    sres_set=true
+fi
